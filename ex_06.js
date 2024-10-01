77a5b505b8ab1922eed6297b9f307b9304ecb465
FloChiche <florian.chicheportiche@epitech.digital>
2024-10-01 14:35:58 +0200

    Update ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,13 +1,11 @@
 const fruitArray = ["Blackberries", "Mango", "Kiwi", "Peaches", "Apple", "Banana"];
 
 function getIndexFromName (name) {
-	return fruitArray.indexOf(name) === -1 ? null : fruitArray.indexOf(name);
+	return fruitArray.indexOf(name) === -1 ? 'null : You do not have this fruit' : fruitArray.indexOf(name);
 }
 function displayResult(result) {
 	document.write(result);
 }
-
-
 // DO NOT TOUCH, this will display the output of your function if you've done it well
-displayResult(getIndexFromName("Apple")); // Remplacez "banana" par n'importe quel fruit pour tester
+displayResult(getIndexFromName("Mango")); // Remplacez "banana" par n'importe quel fruit pour tester
 
