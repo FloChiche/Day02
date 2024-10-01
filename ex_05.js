86a7e7cf3d4e8eca3e69c5dcfabcb652febe430c
FloChiche <florian.chicheportiche@epitech.digital>
2024-10-01 13:57:57 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,30 +1,11 @@
 function getBasketContent () {
-	const fruits = [
-  {name: 'Strawberry', quantity: 3},
-  {name: 'Apple', quantity: 2},
-  {name: 'Lime', quantity: 2},
-  {name: 'Peach', quantity: 1},
-  {name: 'Peat', quantity: 2},
-];
+    const fruits = ["strawberry","strawberry","strawberry","apple","apple","lime","lime","peach","pear","pear"]
+    console.log(fruits.length,"N fruit(s) selected");
+    return fruits
+    }
   
-  let basket = [];
-  let totalFruits = 0;
-  
-  fruits.forEach(fruit => {
-  	for (let i = 0; i < fruit.quantity; i++) {
-    	basket.push(fruit.name);
-      }
-      totalFruits += fruit.quantity;
-      });
-      
-      console.log(`${totalFruits} fruit(s) selected`);
-      
-      return basket;
-      }
-
-
-// DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
-if (typeof getBasketContent === "function") {
-	const fruits = getBasketContent()
-	showMyBasket(fruits)
-}
+  // DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
+  if (typeof getBasketContent === "function") {
+      const fruits = getBasketContent()
+      showMyBasket(fruits)}
+   
