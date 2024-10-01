3d3c3a275c230d38f24a605bbac2304f3ece6fd0
FloChiche <florian.chicheportiche@epitech.digital>
2024-10-01 10:19:01 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,17 @@
+function changebackground() {
+  var magicNumber = 1;
+  let magicNumber = 2;
+  let magicNumber = 3;
+
+  if (magicNumber === 3) { 
+    paintYellowRectangle();
+  }
+  if (magicNumber === 2) {
+    paintGreenRectangle();
+  }
+  if (magicNumber === 1) {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
