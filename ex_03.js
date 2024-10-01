d262ed40e4d78bff6e14d4b8d51a409664f35592
FloChiche <florian.chicheportiche@epitech.digital>
2024-10-01 11:57:28 +0200

    Update ex_03.js

--- ex_03.js
+++ ex_03.js
@@ -1,17 +1,19 @@
 function changebackground() {
-  var magicNumber = 1;
-  let magicNumber = 2;
-  let magicNumber = 3;
+  
 
-  if (magicNumber === 3) { 
-    paintYellowRectangle();
-  }
-  if (magicNumber === 2) {
-    paintGreenRectangle();
-  }
-  if (magicNumber === 1) {
-    paintRedRectangle();
-  }
+{let magicNumber = 1
+	if (magicNumber ===1)
+  	paintYellowRectangle();
 }
 
+{let magicNumber = 2
+	if (magicNumber ===2)
+  	paintGreenRectangle();
+}
+
+{var magicNumber =3; 
+if (magicNumber ===3)
+paintRedRectangle();
+}
+}
 changebackground();
